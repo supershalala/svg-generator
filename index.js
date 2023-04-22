0612aab79f2dd5a5fe7002b23e1e8a22e709279c
Adam Shalala <adamshalala@Adams-Air.localdomain>
2023-04-22 16:07:58 +1000

    inquirer prompts

--- index.js
+++ index.js
@@ -2,5 +2,78 @@ const inquirer = require("inquirer");
 
 const fs = require("fs");
 
+const ShapeOptions = [
 
-d
+    "circle",
+
+    "triangle",
+
+    "square",
+
+ 
+
+]
+
+const initalizeApp = () => {
+
+    inquirer
+    .prompt ([
+
+        {
+            type: 'input',
+            name: 'input',
+            message: 'Enter your text (MAX 3 Characters)',
+            validate: function(input) {
+              if (input.length !== 3) {
+                return 'Input must be 3 characters long.';
+              }
+              return true;
+            }
+          },
+
+
+        {
+
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter text color either a keyword or hex';
+        },
+
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Please pick a shape option',
+            choices: ShapeOptions,
+
+        },
+
+        
+
+
+        {
+            type: 'input',
+            name: 'ShapeColor',
+            message: 'Enter text color either a keyword or hex';
+        },
+
+
+
+
+
+
+
+
+
+    ])
+
+
+
+
+
+
+
+
+
+
+    
+}
